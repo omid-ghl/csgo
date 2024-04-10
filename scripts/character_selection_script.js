90fa96fddcd858b07da60c5c9fb42ec43af30cbc
Omid <omidgholami1@yahoo.com>
2024-04-10 11:06:23 +0330

    [fix] bug fix of character name rules

--- scripts/character_selection_script.js
+++ scripts/character_selection_script.js
@@ -2,45 +2,45 @@
 async function fetchAgentData() {
   try {
     const response = await fetch(
-      'https://bymykel.github.io/CSGO-API/api/en/agents.json',
+      "https://bymykel.github.io/CSGO-API/api/en/agents.json"
     );
     if (!response.ok) {
-      throw new Error('Failed to fetch agent data');
+      throw new Error("Failed to fetch agent data");
     }
     const agents = await response.json();
     return agents;
   } catch (error) {
-    console.error('Error fetching agent data:', error);
+    console.error("Error fetching agent data:", error);
     return [];
   }
 }
 
 async function populateCharacterList(selectedTeam) {
-  const characterListDiv = document.getElementById('character-list');
-  characterListDiv.innerHTML = ''; // Clear previous content
+  const characterListDiv = document.getElementById("character-list");
+  characterListDiv.innerHTML = ""; // Clear previous content
 
   const agents = await fetchAgentData();
   if (!agents || agents.length === 0) {
-    console.error('No agent data found.');
+    console.error("No agent data found.");
     return;
   }
 
   const filteredAgents = agents.filter(
-    agent => agent.team.name.toLowerCase() === selectedTeam.toLowerCase(),
+    (agent) => agent.team.name.toLowerCase() === selectedTeam.toLowerCase()
   );
   if (filteredAgents.length === 0) {
-    console.error('No agents found for the selected team.');
+    console.error("No agents found for the selected team.");
     return;
   }
 
-  filteredAgents.forEach(agent => {
-    const agentDiv = document.createElement('div');
-    agentDiv.classList.add('card'); // Add 'card' class to the div
+  filteredAgents.forEach((agent) => {
+    const agentDiv = document.createElement("div");
+    agentDiv.classList.add("card"); // Add 'card' class to the div
     agentDiv.innerHTML = `
       <img src="${agent.image}" alt="${agent.name}">
       <h3>${agent.name}</h3>
     `;
-    agentDiv.addEventListener('click', function () {
+    agentDiv.addEventListener("click", function () {
       selectCharacter(agent); // Pass the selected agent to the function
     });
     characterListDiv.appendChild(agentDiv);
@@ -49,34 +49,43 @@ async function populateCharacterList(selectedTeam) {
 
 // Function to handle character selection
 function selectCharacter(selectedAgent) {
-  const selectedCard = document.querySelector('.card.selected');
+  const selectedCard = document.querySelector(".card.selected");
   if (selectedCard) {
-    selectedCard.classList.remove('selected'); // Remove selection from previously selected card
+    selectedCard.classList.remove("selected"); // Remove selection from previously selected card
   }
   const clickedCard = event.currentTarget;
-  clickedCard.classList.add('selected'); // Add selection to the clicked card
+  clickedCard.classList.add("selected"); // Add selection to the clicked card
 
   // Save selected character's data and input data in session storage
-  sessionStorage.setItem('selectedCharacter', JSON.stringify(selectedAgent));
+  sessionStorage.setItem("selectedCharacter", JSON.stringify(selectedAgent));
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const team = sessionStorage.getItem('selectedTeam');
+document.addEventListener("DOMContentLoaded", function () {
+  const team = sessionStorage.getItem("selectedTeam");
   if (team) {
     populateCharacterList(team);
   } else {
-    console.error('Team not found in sessionStorage.');
+    console.error("Team not found in sessionStorage.");
   }
 
-  const confirmBtn = document.getElementById('confirm-btn');
-  confirmBtn.addEventListener('click', function () {
-    const characterNameInput = document.getElementById('character-name');
+  const confirmBtn = document.getElementById("confirm-btn");
+  confirmBtn.addEventListener("click", function () {
+    const characterNameInput = document.getElementById("character-name");
+    const characterName = characterNameInput.value.trim();
 
-    sessionStorage.setItem('characterNameInput', characterNameInput.value);
+    // Validate character name
+    if (!isValidCharacterName(characterName)) {
+      alert(
+        "Please enter a valid character name (up to 2 words, letter can up to 20 letters)."
+      );
+      return;
+    }
 
-    const selectedCard = document.querySelector('.card.selected');
+    sessionStorage.setItem("characterNameInput", characterName);
+
+    const selectedCard = document.querySelector(".card.selected");
     if (!selectedCard) {
-      alert('Please select a character.');
+      alert("Please select a character.");
       return;
     }
 
@@ -84,3 +93,22 @@ document.addEventListener('DOMContentLoaded', function () {
     window.location.href = `weapon_selection.html`;
   });
 });
+
+// Function to validate character name
+function isValidCharacterName(name) {
+  // Split the name into words
+  const words = name.split(" ");
+
+  // Check number of words
+  if (words.length > 2) {
+    return false;
+  }
+
+  // Check length of each word
+
+  if (name.length > 20) {
+    return false;
+  }
+
+  return true;
+}
